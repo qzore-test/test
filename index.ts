37ae69851aae062314cef11ed58ab0e4886c6bfc
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-02-13 17:38:48 +0300

    123

---
+++ index.ts
@@ -0,0 +1,20 @@
+import "./Translate"
+
+import { ParticlesX } from "github.com/octarine-private/immortal-core/index"
+import {
+	EventsSDK,
+	LocalPlayer
+} from "github.com/octarine-public/wrapper/index"
+
+
+const pSDK = new ParticlesX()
+
+EventsSDK.on("Draw", () => {
+	if(LocalPlayer.Hero.IsVisibleForEnemies) {
+	pSDK.DrawBox(LocalPlayer.Handle, LocalPlayer, {
+		Position: LocalPlayer.Hero.VisualPosition,
+		Color: 225,
+		Alpha: 100,
+		Radius: 500
+	})}
+})
