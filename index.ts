599c9e75b590a218880076ae4073186944b1294d
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-02-20 17:12:45 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,34 +1,68 @@
+// import "./Translate"
+
+// import { ParticlesX } from "github.com/octarine-private/immortal-core/index"
+// import {
+// 	EventsSDK,
+// 	LocalPlayer,
+// 	monkey_king_tree_dance,
+// 	EntityManager
+// } from "github.com/octarine-public/wrapper/index"
+
+
+// const pSDK = new ParticlesX()
+
+// EventsSDK.on("Draw", () => {
+// 	const MyHero = LocalPlayer?.Hero
+// 	if(LocalPlayer.Hero.IsVisibleForEnemies) {
+// 	pSDK.DrawBox(LocalPlayer.Handle, LocalPlayer, {
+// 		Position: LocalPlayer.Hero.VisualPosition,
+// 		Color: 225,
+// 		Alpha: 100,
+// 		Radius: 500
+// 	})
+// 	// EntityManager.GetEntitiesByClass(monkey_king_tree_dance).some(abil => {
+// 	// 	console.log(abil)	
+// 		const castAbil = MyHero.GetItemByName('item_quelling_blade')
+// 		console.log(castAbil)
+// 	// })
+// }})
+
+// // next step
+// // EventsSDK.on("UnitVisibilityChanged", () => {
+// // console.log("npc")
+// // })
+
+
+
 import "./Translate"
 
-import { ParticlesX } from "github.com/octarine-private/immortal-core/index"
+// import { ParticlesX } from "github.com/octarine-private/immortal-core/index"
 import {
 	EventsSDK,
 	LocalPlayer,
-	monkey_king_tree_dance,
-	EntityManager
+	RendererSDK,
+	Vector2
 } from "github.com/octarine-public/wrapper/index"
 
 
-const pSDK = new ParticlesX()
-
+// const pSDK = new ParticlesX()
+const test = new RendererSDK()
+const vec2 = new Vector2()
 EventsSDK.on("Draw", () => {
-	const MyHero = LocalPlayer?.Hero
-	if(LocalPlayer.Hero.IsVisibleForEnemies) {
-	pSDK.DrawBox(LocalPlayer.Handle, LocalPlayer, {
-		Position: LocalPlayer.Hero.VisualPosition,
-		Color: 225,
-		Alpha: 100,
-		Radius: 500
-	})
-	// EntityManager.GetEntitiesByClass(monkey_king_tree_dance).some(abil => {
-	// 	console.log(abil)	
-		const castAbil = MyHero.GetItemByName('item_quelling_blade')
-		console.log(castAbil)
-	// })
-}})
+	test.Text({
+		text: 'tt',
+		vecPos: LocalPlayer.Hero.Position,
+	}
+	)
 
-// next step
-// EventsSDK.on("UnitVisibilityChanged", () => {
-// console.log("npc")
-// })
 
+
+
+	// if(LocalPlayer.Hero.IsVisibleForEnemies) {
+	// pSDK.DrawBox(LocalPlayer.Handle, LocalPlayer, {
+	// 	Position: LocalPlayer.Hero.VisualPosition,
+	// 	Color: 225,
+	// 	Alpha: 100,
+	// 	Radius: 500
+	// })}
+})
