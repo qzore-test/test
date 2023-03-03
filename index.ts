37131a255c4275ce780e614f518a2182464cbfab
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-03-03 22:12:46 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,16 +4,17 @@ import { Color, EventsSDK, GUIInfo, RendererSDK, Vector2, Vector3, LocalPlayer, 
 // seleeper.Sleep(50)
 EventsSDK.on("Draw", () => {
 	const kek = LocalPlayer.Hero.VisualPosition.x
-	const kek1 = LocalPlayer.Hero.VisualPosition
+	const kek1 = LocalPlayer.Hero.VisualPosition.y
 
 	const text = `${kek}`
 	const w2sPosition = RendererSDK.WorldToScreen(LocalPlayer.Hero.VisualPosition)
 	
 	const vectorSize = new Vector2()
 	const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
-
+	const vectorSize1 = new Vector2(kek, kek1)
 		RendererSDK.Line(
-			position
+			position,
+			vectorSize1
 			)
 		
 		// RendererSDK.Text(
