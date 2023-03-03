0988db67ecff41a3001792fb1d03ca2873534cae
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-03-03 08:31:40 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,19 +1,21 @@
 import { Color, EventsSDK, GUIInfo, RendererSDK, Vector2, Vector3, LocalPlayer } from "github.com/octarine-public/wrapper/index"
-
-
+let pepe = 'asd'
+const text = `${pepe} TY LOX, a YA NET`
+setInterval(() => {
+	pepe += 1
+}, 1000);
 EventsSDK.on("Draw", () => {
 	const w2sPosition = RendererSDK.WorldToScreen(LocalPlayer.Hero.VisualPosition)
 
 	// console.log(w2sPosition)
 
 	const vectorSize = new Vector2(GUIInfo.ScaleWidth(0), GUIInfo.ScaleWidth(0))
-	const vectorkek = new Vector2(-2488, 1271)
 	const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
-	const text = 'TY LOX, a YA NET'
 		RendererSDK.Text(
 			text,
 			position
 		)
+
 		// RendererSDK.Image(
 		// 	"panorama/images/spellicons/modifier_illusion_png.vtex_c",
 		// 	position,
@@ -21,6 +23,7 @@ EventsSDK.on("Draw", () => {
 		// 	vectorSize,
 		// )
 	})
+
 		// RendererSDK.OutlinedCircle(
 		// 	position,
 		// 	vectorSize,
