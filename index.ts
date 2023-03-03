1a707041d564df332bb10d9252e33fc8a01a85f4
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-03-03 08:50:17 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import { Color, EventsSDK, GUIInfo, RendererSDK, Vector2, Vector3, LocalPlayer, TickSleeper } from "github.com/octarine-public/wrapper/index"
 
-const seleeper = new TickSleeper()
-
-
 EventsSDK.on("Draw", () => {
+	const seleeper = new TickSleeper()
 	const text = `${seleeper.lastSleepTickCount} TY LOX, a YA NET`
 	const w2sPosition = RendererSDK.WorldToScreen(LocalPlayer.Hero.VisualPosition)
 
