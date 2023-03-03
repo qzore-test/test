bfcfc221e1f4050bedd20c6c00de2e5af7ec0df2
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-03-03 06:17:34 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,68 +1,20 @@
-// import "./Translate"
-
-// import { ParticlesX } from "github.com/octarine-private/immortal-core/index"
-// import {
-// 	EventsSDK,
-// 	LocalPlayer,
-// 	monkey_king_tree_dance,
-// 	EntityManager
-// } from "github.com/octarine-public/wrapper/index"
-
-
-// const pSDK = new ParticlesX()
-
-// EventsSDK.on("Draw", () => {
-// 	const MyHero = LocalPlayer?.Hero
-// 	if(LocalPlayer.Hero.IsVisibleForEnemies) {
-// 	pSDK.DrawBox(LocalPlayer.Handle, LocalPlayer, {
-// 		Position: LocalPlayer.Hero.VisualPosition,
-// 		Color: 225,
-// 		Alpha: 100,
-// 		Radius: 500
-// 	})
-// 	// EntityManager.GetEntitiesByClass(monkey_king_tree_dance).some(abil => {
-// 	// 	console.log(abil)	
-// 		const castAbil = MyHero.GetItemByName('item_quelling_blade')
-// 		console.log(castAbil)
-// 	// })
-// }})
-
-// // next step
-// // EventsSDK.on("UnitVisibilityChanged", () => {
-// // console.log("npc")
-// // })
-
-
-
 import "./Translate"
 
-// import { ParticlesX } from "github.com/octarine-private/immortal-core/index"
+import { ParticlesX } from "github.com/octarine-private/immortal-core/index"
 import {
 	EventsSDK,
 	LocalPlayer,
-	RendererSDK,
-	Vector2
 } from "github.com/octarine-public/wrapper/index"
 
 
-// const pSDK = new ParticlesX()
-const vec2 = new Vector2()
+const pSDK = new ParticlesX()
+
 EventsSDK.on("Draw", () => {
-Text({
-		text: '213dsfa',
-		vecPos: vec2,
-})
-	}
-	)
-
-
-
-
-	// if(LocalPlayer.Hero.IsVisibleForEnemies) {
-	// pSDK.DrawBox(LocalPlayer.Handle, LocalPlayer, {
-	// 	Position: LocalPlayer.Hero.VisualPosition,
-	// 	Color: 225,
-	// 	Alpha: 100,
-	// 	Radius: 500
-	// })}
-// })
+	if(LocalPlayer.Hero.IsVisibleForEnemies) {
+	pSDK.DrawBox(LocalPlayer.Handle, LocalPlayer, {
+		Position: LocalPlayer.Hero.VisualPosition,
+		Radius: 500,
+		Color: 225,
+		Alpha: 100
+	})
+	}})
