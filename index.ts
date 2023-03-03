1b415da682469b044df8f374f2796633e457d715
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-03-03 07:28:29 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,40 +1,7 @@
 import { Color, GUIInfo, RendererSDK, Vector2, Vector3 } from "github.com/octarine-public/wrapper/index"
 
-import { DrawTypeIllusion } from "../Enum/DrawType"
 
-export interface IMenu {
-	State: boolean
-	Size: number
-	Opacity: number
-	Type: DrawTypeIllusion
-}
 
-export interface IBaseDrawable {
-	Key: any
-	Menu: IMenu
-	IsVisible: boolean
-	PlayerColor: Color
-	Position: () => Vector3
-}
-
-export class BaseDrawable {
-	constructor(public readonly option: IBaseDrawable) {}
-
-	public get Position() {
-		return this.option.Position()
-	}
-
-	public get Menu() {
-		return this.option.Menu
-	}
-
-	public get PlayerColor() {
-		return this.option.PlayerColor
-	}
-
-	public get IsVisible() {
-		return this.option.IsVisible
-	}
 
 	public OnDraw() {
 		console.log('123')
@@ -65,5 +32,3 @@ export class BaseDrawable {
 		// RendererSDK.FilledCircle(position, vectorSize, Color.Yellow.SetA(Opacity))
 		// RendererSDK.OutlinedCircle(position, vectorSize, this.PlayerColor.SetA(Opacity), GUIInfo.ScaleHeight(Size) / 15)
 	}
-
-}
