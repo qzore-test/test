36213c31bdebf8dcd0161e8cea1bbfeb9f7a0727
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-03-03 07:08:03 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,73 @@
-import "./Translate"
+import { Color, GUIInfo, RendererSDK, Vector2, Vector3 } from "github.com/octarine-public/wrapper/index"
 
-import { ParticlesX } from "github.com/octarine-private/immortal-core/index"
-import {
-	EventsSDK,
-	RendererSDK,
-	LocalPlayer,
-	Color
-} from "github.com/octarine-public/wrapper/index"
+import { DrawTypeIllusion } from "../Enum/DrawType"
+
+export interface IMenu {
+	State: boolean
+	Size: number
+	Opacity: number
+	Type: DrawTypeIllusion
+}
+
+export interface IBaseDrawable {
+	Key: any
+	Menu: IMenu
+	IsVisible: boolean
+	PlayerColor: Color
+	Position: () => Vector3
+}
+
+export class BaseDrawable {
+	constructor(public readonly option: IBaseDrawable) {}
+
+	public get Position() {
+		return this.option.Position()
+	}
+
+	public get Menu() {
+		return this.option.Menu
+	}
+
+	public get PlayerColor() {
+		return this.option.PlayerColor
+	}
+
+	public get IsVisible() {
+		return this.option.IsVisible
+	}
+
+	public OnDraw() {
+		if (!this.Menu.State || !this.IsVisible) {
+			return
+		}
+
+		const w2sPosition = RendererSDK.WorldToScreen(this.Position)
+		if (w2sPosition === undefined) {
+			return
+		}
+
+		const vectorSize = new Vector2(GUIInfo.ScaleWidth(50), GUIInfo.ScaleWidth(50))
+
+		const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
+
+			RendererSDK.Image(
+				"panorama/images/spellicons/modifier_illusion_png.vtex_c",
+				position,
+				0,
+				vectorSize,
+				Color.White.SetA(100)
+			)
+			// RendererSDK.OutlinedCircle(
+			// 	position,
+			// 	vectorSize,
+			// 	this.PlayerColor.SetA(Opacity),
+			// 	GUIInfo.ScaleHeight(Size) / 15
+			// )
+			// return
 
 
-const pSDK = new ParticlesX()
-EventsSDK.on("Draw", () => {
-	const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
-	console.log(position)
-	})
+		// RendererSDK.FilledCircle(position, vectorSize, Color.Yellow.SetA(Opacity))
+		// RendererSDK.OutlinedCircle(position, vectorSize, this.PlayerColor.SetA(Opacity), GUIInfo.ScaleHeight(Size) / 15)
+	}
+
+}
