a6a09ff33208512140c4886176dfb0c6f990f7f2
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-03-03 07:52:12 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,16 +2,16 @@ import { Color, EventsSDK, GUIInfo, RendererSDK, Vector2, Vector3, LocalPlayer }
 
 
 EventsSDK.on("Draw", () => {
-	const w2sPosition = RendererSDK.WorldToScreen(LocalPlayer.Hero.VisualPosition)
+	// const w2sPosition = RendererSDK.WorldToScreen(LocalPlayer.Hero.VisualPosition)
 
 	// console.log(w2sPosition)
 
-	const vectorSize = new Vector2(GUIInfo.ScaleWidth(300), GUIInfo.ScaleWidth(300))
+	// const vectorSize = new Vector2(GUIInfo.ScaleWidth(300), GUIInfo.ScaleWidth(300))
 	const vectorkek = new Vector2(-2488, 1271)
-	const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
-
+	// const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
+	const text = 'pepega'
 		RendererSDK.Text(
-			'kekw',
+			text,
 			vectorkek
 		)
 		// RendererSDK.Image(
