32e9a87f37fd12d77837e2fea9d9d95df80b246d
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-03-03 07:36:03 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,34 +1,32 @@
-import { Color, GUIInfo, RendererSDK, Vector2, Vector3 } from "github.com/octarine-public/wrapper/index"
+import { Color, EventsSDK, GUIInfo, RendererSDK, Vector2, Vector3, LocalPlayer } from "github.com/octarine-public/wrapper/index"
 
 
+EventsSDK.on("Draw", () => {
+	console.log('123')
+	const w2sPosition = RendererSDK.WorldToScreen(LocalPlayer.Hero.VisualPosition)
+
+	console.log(w2sPosition)
+
+	const vectorSize = new Vector2(GUIInfo.ScaleWidth(100), GUIInfo.ScaleWidth(100))
+
+	const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
+
+		RendererSDK.Image(
+			"panorama/images/spellicons/modifier_illusion_png.vtex_c",
+			position,
+			0,
+			vectorSize,
+			Color.White.SetA(50)
+		)
+	})
+		// RendererSDK.OutlinedCircle(
+		// 	position,
+		// 	vectorSize,
+		// 	this.PlayerColor.SetA(Opacity),
+		// 	GUIInfo.ScaleHeight(Size) / 15
+		// )
+		// return
 
 
-	public OnDraw() {
-		console.log('123')
-		const w2sPosition = RendererSDK.WorldToScreen(this.Position)
-
-		console.log(w2sPosition + '123')
-
-		const vectorSize = new Vector2(GUIInfo.ScaleWidth(100), GUIInfo.ScaleWidth(100))
-
-		const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
-
-			RendererSDK.Image(
-				"panorama/images/spellicons/modifier_illusion_png.vtex_c",
-				position,
-				0,
-				vectorSize,
-				Color.White.SetA(50)
-			)
-			// RendererSDK.OutlinedCircle(
-			// 	position,
-			// 	vectorSize,
-			// 	this.PlayerColor.SetA(Opacity),
-			// 	GUIInfo.ScaleHeight(Size) / 15
-			// )
-			// return
-
-
-		// RendererSDK.FilledCircle(position, vectorSize, Color.Yellow.SetA(Opacity))
-		// RendererSDK.OutlinedCircle(position, vectorSize, this.PlayerColor.SetA(Opacity), GUIInfo.ScaleHeight(Size) / 15)
-	}
+	// RendererSDK.FilledCircle(position, vectorSize, Color.Yellow.SetA(Opacity))
+	// RendererSDK.OutlinedCircle(position, vectorSize, this.PlayerColor.SetA(Opacity), GUIInfo.ScaleHeight(Size) / 15
