511bd66734b9b55c4bca4af489acdd2f8d031ad6
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-03-04 10:37:57 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,29 +3,30 @@ import { Color, EventsSDK, GUIInfo, RendererSDK, Vector2, Vector3, LocalPlayer, 
 EventsSDK.on("Draw", () => {
 	const kek = LocalPlayer.Hero.VisualPosition
 
-	const text = `${kek}`
 	if (LocalPlayer.Hero.HPRegen < 3)
 	{
-	const w2sPosition = RendererSDK.WorldToScreen(LocalPlayer.Hero.VisualPosition)
-	const w2sPosition1 = RendererSDK.WorldToScreen(new Vector2(-1763.5, 1172.40625))
-	
-	const vectorSize = new Vector2()
-	const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
-	const positionEnd = w2sPosition1.Subtract(vectorSize.DivideScalar(2))
-	const MyHero = LocalPlayer?.Hero
-	const kekus = MyHero.GetItemByClass(item_aeon_disk)
-	console.log(kekus)
-			RendererSDK.Line(
-				position,
-				positionEnd
-				)
-		}
+		const w2sPosition = RendererSDK.WorldToScreen(LocalPlayer.Hero.VisualPosition)
+		const w2sPosition1 = RendererSDK.WorldToScreen(new Vector2(-1763.5, 1172.40625))
+		
+		const vectorSize = new Vector2()
+		const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
+		// const positionEnd = w2sPosition1.Subtract(vectorSize.DivideScalar(2))
+		const MyHero = LocalPlayer?.Hero
+		// const kekus = MyHero.GetItemByClass(item_aeon_disk)
+		const currentHp = LocalPlayer?.Hero.HP
+		const matHp = currentHp - (currentHp / 100 * 70)
+		const text = `${matHp}`
+		// 	RendererSDK.Line(
+		// 		position,
+		// 		positionEnd
+		// 		)
+		// }
 		
 		
-		// RendererSDK.Text(
-		// 	text,
-		// 	position
-		// )
+		RendererSDK.Text(
+			text,
+			position
+		)
 
 		// RendererSDK.Image(
 		// 	"panorama/images/spellicons/modifier_illusion_png.vtex_c",
