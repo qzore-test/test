eb0eb9fe49690e33c3f6885757dd4cd495edb097
qzore-test <dsafsgfjhb121123af@mail.ru>
2023-03-04 04:56:20 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
 import { Color, EventsSDK, GUIInfo, RendererSDK, Vector2, Vector3, LocalPlayer } from "github.com/octarine-public/wrapper/index"
-const kek = LocalPlayer.Hero.VisualPosition
 
-const text = `${kek}`
-const w2sPosition = RendererSDK.WorldToScreen(LocalPlayer.Hero.VisualPosition)
-const w2sPosition1 = RendererSDK.WorldToScreen(new Vector2(-1763.5, 1172.40625))
+EventsSDK.on("Draw", () => {
+	const kek = LocalPlayer.Hero.VisualPosition
 
-const vectorSize = new Vector2()
-const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
-const positionEnd = w2sPosition1.Subtract(vectorSize.DivideScalar(2))
+	const text = `${kek}`
+	const w2sPosition = RendererSDK.WorldToScreen(LocalPlayer.Hero.VisualPosition)
+	const w2sPosition1 = RendererSDK.WorldToScreen(new Vector2(-1763.5, 1172.40625))
+	
+	const vectorSize = new Vector2()
+	const position = w2sPosition.Subtract(vectorSize.DivideScalar(2))
+	const positionEnd = w2sPosition1.Subtract(vectorSize.DivideScalar(2))
 
-	RendererSDK.Line(
-		position,
-		positionEnd
-		)
-// EventsSDK.on("Draw", () => {
-
+		RendererSDK.Line(
+			position,
+			positionEnd
+			)
 		
 		// RendererSDK.Text(
 		// 	text,
@@ -27,7 +27,7 @@ const positionEnd = w2sPosition1.Subtract(vectorSize.DivideScalar(2))
 		// 	0,
 		// 	vectorSize,
 		// )
-	// })
+	})
 
 		// RendererSDK.OutlinedCircle(
 		// 	position,
